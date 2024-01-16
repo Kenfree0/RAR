9e521373f707d659e6cce993a6647200f15fe89a
Kenfree0 <398872073@qq.com>
2024-01-16 17:19:17 +0800

    Update run_train.py

--- scripts/run_train.py
+++ scripts/run_train.py
@@ -24,18 +24,18 @@ if __name__ == '__main__':
     parser.add_argument('--data_dir', type=str, default='', help='path to training dataset')
     parser.add_argument('--data_split_num', type=int, default=0, help='fold number of training dataset')
 
-    parser.add_argument('--noise_schedule', type=str, default='cosine', choices=['linear', 'cosine', 'sqrt', 'trunc_cos', 'trunc_lin', 'pw_lin', 'warmup-decay'], help='the distribution of noises')
-    parser.add_argument('--diff_steps', type=int, default=4000, help='diffusion steps')
-    parser.add_argument('--schedule_sampler', type=str, default='uniform', choices=['uniform', 'lossaware', 'fixstep'], help='schedule sampler of timesteps')
-    parser.add_argument('--learned_mean_embed', default=False, type=str2bool, help='learn mean embed for gaussian; default is zero-mean gaussian')
-    parser.add_argument('--denoise', default=False, type=str2bool, help='denoise combined with learned_mean_embed [MASK]')
-    parser.add_argument('--reg_rate', default=0.0, type=float, help='regularization rate of learned mean embed for gaussian; default is zero')
-    parser.add_argument('--denoise_rate', default=0.2, type=float, help='max denoise rate of [MASK]')
+    parser.add_argument('--schedule', type=str, default='cosine', choices=['linear', 'cosine', 'sqrt', 'trunc_cos', 'trunc_lin', 'pw_lin', 'warmup-decay'])
+    parser.add_argument('--steps', type=int, default=4000, help='steps')
+    parser.add_argument('--sampler', type=str, default='uniform', choices=['uniform', 'lossaware', 'fixstep'])
+    parser.add_argument('--learned_mean_embed', default=False, type=str2bool)
+    parser.add_argument('--decode', default=False, type=str2bool)
+    parser.add_argument('--reg_rate', default=0.0, type=float, )
+    parser.add_argument('--decode_rate', default=0.2, type=float, help='max decode rate of [MASK]')
 
     parser.add_argument('--seq_len', type=int, default=128, help='max len of input sequence')
-    parser.add_argument('--hidden_t_dim', type=int, default=128, help='hidden size of time embedding')
+    parser.add_argument('--hiddecon_t_dim', type=int, default=128, help='hidden size of time embedding')
     parser.add_argument('--hidden_dim', type=int, default=128, help='hidden size of word embedding')
-    parser.add_argument('--learning_steps', type=int, default=40000, help='total steps of learning')
+    parser.add_argument('--learning_steps', type=int, default=400, help='total steps of learning')
     parser.add_argument('--save_interval', type=int, default=10000, help='save step')
     parser.add_argument('--resume_checkpoint', type=str, default='none', help='path to resume checkpoint, like xxx/xxx.pt')
     parser.add_argument('--lr', type=float, default=1e-04, help='learning rate')
@@ -52,14 +52,12 @@ if __name__ == '__main__':
     parser.add_argument('--app', type=str, default='', help='other input args')
     
     args = parser.parse_args()
-
-    # set working dir to the upper folder
     abspath = os.path.abspath(sys.argv[0])
     dname = os.path.dirname(abspath)
     dname = os.path.dirname(dname)
     os.chdir(dname)
 
-    folder_name = "diffusion_models/"
+    folder_name = "models/"
 
     if int(os.environ['LOCAL_RANK']) == 0:
         if not os.path.isdir(folder_name):
@@ -69,7 +67,6 @@ if __name__ == '__main__':
                 f"_t{args.diff_steps}_{args.noise_schedule}_{args.schedule_sampler}" \
                 f"_seed{args.seed}"
     if args.notes:
-        #args.notes += time.strftime("%Y%m%d-%H:%M:%S")
         Model_FILE = Model_FILE + f'_{args.notes}'
     Model_FILE = os.path.join(folder_name, Model_FILE)
 
@@ -82,9 +79,9 @@ if __name__ == '__main__':
                   f"--dataset {args.dataset} --data_dir {args.data_dir} --data_split_num {args.data_split_num} --vocab {args.vocab} --use_plm_init {args.use_plm_init} " \
                   f"--lr {args.lr} --use_fp16 {args.use_fp16} " \
                   f"--batch_size {args.bsz} --microbatch {args.microbatch} " \
-                  f"--diffusion_steps {args.diff_steps} " \
-                  f"--noise_schedule {args.noise_schedule} " \
-                  f"--schedule_sampler {args.schedule_sampler} --resume_checkpoint {args.resume_checkpoint} " \
+                  f"--steps {args.diff_steps} " \
+                  f"--schedule {args.noise_schedule} " \
+                  f"--sampler {args.schedule_sampler} --resume_checkpoint {args.resume_checkpoint} " \
                   f"--seq_len {args.seq_len} --hidden_t_dim {args.hidden_t_dim} --seed {args.seed} " \
                   f"--hidden_dim {args.hidden_dim} " \
                   f"--learning_steps {args.learning_steps} --save_interval {args.save_interval} " \
